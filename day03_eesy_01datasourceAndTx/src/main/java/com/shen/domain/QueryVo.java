07d3bcf5c313ca98f36c37d2be86106a86c43927
shenyiyi <2994478706@qq.com>
2020-07-07 19:04:09 +0800

    添加本地学习文件

---
+++ day03_eesy_01datasourceAndTx/src/main/java/com/shen/domain/QueryVo.java
@@ -0,0 +1,16 @@
+package com.shen.domain;
+
+import com.shen.domain.User;
+
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
